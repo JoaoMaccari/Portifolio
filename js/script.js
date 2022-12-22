d5b7e7921eccc80a6ee795aba8dc8e67a633dd3b
João Elias Maccari <joaomaccari@hotmail.com>
2022-12-21 21:12:45 -0300

    att mediaQ

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function abrirmenu(){
     let BarraMenuAberto = document.getElementById('barra-menu')
     if(BarraMenuAberto.style.width == "0px"){
         BarraMenuAberto.style.width = "180px";
+        BarraMenuAberto.style.height = "62vh";
     }else{
         BarraMenuAberto.style.width = "0px";
     }
